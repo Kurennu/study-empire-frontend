7dfa6ac137fa9edc3ac66243fa9bf6b45d3a8b0b
Kurennu <kurenu66@gmail.com>
2025-04-11 19:24:49 +1000

    change: js imports

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-// Импорт компонентов
 import Header from './Header.js'
 import CourseCards from './CourseCards.js'
 import Carousel from './Carousel.js'
@@ -6,12 +5,10 @@ import Review from './Review.js'
 import Form from './Form.js'
 import Quiz from './Quiz.js'
 
-// Инициализация компонентов при загрузке страницы
 document.addEventListener('DOMContentLoaded', () => {
     new Header()
     new CourseCards()
     new Carousel()
     new Review()
     new Form()
-    // Quiz инициализируется внутри своего модуля при загрузке страницы
 })
